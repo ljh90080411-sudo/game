9c6942041343b9c429d011fcb0ecaa39569adcb7
ljh90080411-sudo <ljh90080411@gmail.com>
2026-08-09 23:53:11 +0900

    Update app.py

--- ocr-server/app.py
+++ ocr-server/app.py
@@ -30,14 +30,14 @@ _ocr = None
 def get_ocr():
     global _ocr
     if _ocr is None:
-        from rapidocr import RapidOCR
+        from rapidocr import RapidOCR, LangDet, LangRec, ModelType
 
         _ocr = RapidOCR(
             params={
-                "Det.lang_type": "ch",
-                "Det.model_type": "mobile",
-                "Rec.lang_type": "korean",
-                "Rec.model_type": "mobile",
+                "Det.lang_type": LangDet.CH,
+                "Det.model_type": ModelType.MOBILE,
+                "Rec.lang_type": LangRec.KOREAN,
+                "Rec.model_type": ModelType.MOBILE,
             }
         )
     return _ocr
